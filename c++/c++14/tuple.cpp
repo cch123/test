d1275463e38ff14d89e28b3acf5a1e977254e748
Xargin <cao1988228@163.com>
2016-11-09 23:37:39 +0800

    tuple example

---
+++ c++/c++14/tuple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    tuple<int, bool, float, string> result = make_tuple(128, true, 1.1f, "no");
+    auto value = get<0>(result);
+    auto value1 = get<1>(result);
+    auto value2 = get<2>(result);
+
+    int obj1;
+    bool obj2;
+    float obj3;
+    tie(obj1, obj2, obj3) = make_tuple(128, true, 1.2f);
+    cout << obj1 << " " << obj2 << " " << obj3 << endl;
+    cout << value << endl;
+    cout << value1 << endl;
+    cout << value2 << endl;
+}
