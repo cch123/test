f52ddd8bc9ab40aca297a0b08b01572a9e5e3182
Xargin <cao1988228@163.com>
2016-12-16 20:40:39 +0800

    standard lib middleware

--- go/middleware/standard_middleware.go
+++ go/middleware/standard_middleware.go
@@ -2,14 +2,22 @@ package main
 
 import "net/http"
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+type mainHandler struct{}
+
+func (m mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world"))
 }
 
-func hookHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("yes in hook"))
+func hookHandler(next http.Handler) http.Handler {
+	//是HandlerFunc而不是HandleFunc，注意。。
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("in hook"))
+		next.ServeHTTP(w, r)
+		w.Write([]byte("in hook after"))
+	})
 }
 
 func main() {
-	http.ListenAndServe(":8080", mainHandler)
+	var a mainHandler
+	http.ListenAndServe(":8080", hookHandler(a))
 }
