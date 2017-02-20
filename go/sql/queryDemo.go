5551645becbfeda9deacc74ba64c7799d3c33638
Xargin <cao1988228@163.com>
2017-02-20 17:23:50 +0800

    add db sql query demo

---
+++ go/sql/queryDemo.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+)
+
+// Person means Person
+type Person struct {
+	age  int
+	name string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@/test")
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		return
+	}
+
+	var p Person
+
+	rows, err := db.Query(`select * from abc where id = ?`, 10)
+	err2 := errors.Wrap(err, "main")
+	//err3 := errors.WithStack(err)
+
+	if err != nil {
+		fmt.Printf("%#v\n", errors.Cause(err2))
+		fmt.Printf("%+v\n", err2)
+		fmt.Printf("%s\n", err2)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		rows.Scan(&p.name, &p.age)
+	}
+
+}
