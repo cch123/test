c370c32cb97dc4aae9ed20f3d98f49be15250a73
Xargin <cao1988228@163.com>
2017-05-26 00:07:15 +0800

    add reflect

---
+++ go/dirty_part/aaa.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Aer interface {
+	Name() string
+	PrintName()
+}
+
+type A struct {
+}
+
+func (a *A) Name() string {
+	return "a"
+}
+
+func (a *A) PrintName() {
+	fmt.Println(a.Name())
+}
+
+type B struct {
+	A
+}
+
+func (b *B) Name() string {
+	return "b"
+}
+
+func getAer() Aer {
+	return &B{}
+}
+
+func main() {
+	a := getAer()
+	a.PrintName()
+}
