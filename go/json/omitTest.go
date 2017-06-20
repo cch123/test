a74fc7b4a2c7d83f988d1c4f1818a2d068fe6e37
Xargin <cao1988228@163.com>
2017-06-20 14:37:04 +0800

    update omitempty test

--- go/json/omitTest.go
+++ go/json/omitTest.go
@@ -6,15 +6,23 @@ import (
 )
 
 type queryRequest struct {
+	// 0，空字符串，nil指针
 	Query string `json:"query,omitempty"`
 	From  string `json:"from,omitempty"`
 	Size  string `json:"size,omitempty"`
 	Sort  string `json:"sort,omitempty"`
+	X     int    `json:"x,omitempty"`
+	Y     []int  `json:"y,omitempty"`
 }
 
 func main() {
 	var a queryRequest
 	a.Query = "fuck"
+
+	// 加不加这句都不会输出的
+	a.Y = []int{}
+
 	byteRes, _ := json.Marshal(a)
 	fmt.Println(string(byteRes))
+	fmt.Printf("%#v\n", a)
 }
