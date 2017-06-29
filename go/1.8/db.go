8f74eec29fcade9f80f63c1fe023b87196ea86ed
Xargin <cao1988228@163.com>
2017-06-29 19:55:44 +0800

    add new file

---
+++ go/1.8/db.go
@@ -0,0 +1,13 @@
+//cancelable queries
+//visible database types
+//multiple result sets
+//ping can hit server
+//named parameters
+//transaction isolation levels
+// https://docs.google.com/document/d/1F778e7ZSNiSmbju3jsEWzShcb8lIO4kDyfKDNm4PNd8/edit#
+
+package main
+
+func main() {
+
+}
