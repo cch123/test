54e6c0ca7e1093e0b03d76ecd8bcd7b49684241d
Xargin <cao1988228@163.com>
2017-07-02 02:49:59 +0800

    add msgpack

---
+++ go/msgpack/t.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+func main() {
+	type Item struct {
+		Foo string
+	}
+
+	b, err := msgpack.Marshal(&Item{Foo: "bar"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
+
+	var item Item
+	err = msgpack.Unmarshal(b, &item)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(item.Foo)
+	// Output: bar
+}
