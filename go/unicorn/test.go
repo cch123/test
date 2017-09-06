a5fb8923c4277e78dfffbe308f0114044733a811
Xargin <cao1988228@163.com>
2017-09-06 22:51:43 +0800

    add readme for unicorn

--- go/unicorn/test.go
+++ go/unicorn/test.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/hex"
 
 	uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
 )
 
 func main() {
 	mu, _ := uc.NewUnicorn(uc.ARCH_X86, uc.MODE_64)
-	// mov eax, 1234
-	code := []byte{184, 210, 4, 0, 0}
+	// mov rax, 0x1234
+	// code, _ := hex.DecodeString("48c7c034120000")
+
+	// push rax
+	//code, _ := hex.DecodeString("50")
+	code, _ := hex.DecodeString("58")
+	// push rax
 	mu.MemMap(0x1000, 0x1000)
 	mu.MemWrite(0x1000, code)
+	mu.RegWrite(uc.X86_REG_RBP, 0x2000)
+	mu.RegWrite(uc.X86_REG_RSP, 0x1200)
 	if err := mu.Start(0x1000, 0x1000+uint64(len(code))); err != nil {
 		panic(err)
 	}
-	err := mu.RegWrite(uc.X86_REG_R10, 10101010101010101010)
-	fmt.Println(err)
-	eax, _ := mu.RegRead(uc.X86_REG_EAX)
-	r10, err := mu.RegRead(uc.X86_REG_R10)
-	fmt.Printf("EAX is now: %d\n", eax)
-	fmt.Printf("r10 is now: %d\n", r10)
-	fmt.Println(err)
 }
