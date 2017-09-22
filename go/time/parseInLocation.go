1ae38c10589b95312495ea6ea4d0f11d04a5cf6f
Xargin <cao1988228@163.com>
2017-09-22 14:44:50 +0800

    add dateparse example

--- go/time/parseInLocation.go
+++ go/time/parseInLocation.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
-	a := time.Now()
-	rateDate, _ := time.ParseInLocation("2006-01-02", a.Format("2006-01-02"), time.Local)
-	fmt.Println(rateDate)
+	//	a := time.Now()
+	rateDate, _ := time.ParseInLocation("2006-01-02", "2015-01-02", time.Local)
+
+	fmt.Println(rateDate, rateDate.Format("20060102 15:04:05"))
+	r, _ := time.Parse("20060102", "20150102")
+	fmt.Println(r, r.Format("20060102 15:04:05"))
 	//fmt.Printf("%#v", time.Local)
-	var x = 10
+	//	var x = 10
 	var b = time.Minute * time.Duration(100)
 	fmt.Println(rateDate.Add(b))
 
