f93fbc420dce21efed0c77eaeb272982f9b232a3
Xargin <cao1988228@163.com>
2017-12-14 10:36:09 +0800

    change

--- go/channel/channelLen.go
+++ go/channel/channelLen.go
@@ -4,6 +4,12 @@ func main() {
 	ch := make(chan int, 100)
 	ch <- 1
 	ch <- 1
+	for i := 0; i < 100; i++ {
+
+		go func() {
+			<-ch
+		}()
+	}
 	ch <- 1
 	ch <- 1
 	println(len(ch))
--- go/cryptoBench/bcrypt.go
+++ go/cryptoBench/bcrypt.go
@@ -10,13 +10,14 @@ func test(cost int, id int) {
 	code, _ := bcrypt.GenerateFromPassword([]byte("password"), cost)
 	fmt.Println(time.Now(), "END test ", id, code[0])
 }
+
 func main() {
 	runtime.GOMAXPROCS(1)
 	for i := 0; i < 5; i++ {
-		fmt.Println(time.Now(), "BEGIN test ", i)
-		code, _ := bcrypt.GenerateFromPassword([]byte("password"), 14)
-		fmt.Println(time.Now(), "END test ", i, code[0])
-		//	go test(17, i)
+		//fmt.Println(time.Now(), "BEGIN test ", i)
+		//code, _ := bcrypt.GenerateFromPassword([]byte("password"), 17)
+		//fmt.Println(time.Now(), "END test ", i, code[0])
+		go test(15, i)
 	}
 	time.Sleep(1e16)
 }
