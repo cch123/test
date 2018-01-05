ca48fa90cf2b3ac4ca67c16fec246ecacd9b655c
Xargin <cao1988228@163.com>
2018-01-05 23:42:45 +0800

    add new

---
+++ go_web/httprouter_learn.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/cch123/httprouter"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	r := httprouter.New()
+	r.GET("/hello/over/:fuc/set/:name/:id", Hello)
+	//r.GET("/hello/:fuc/:name", Hello)
+	//	r.GET("/hello/fuci/:name", Hello)
+	httprouter.WalkPrint(r, "GET")
+}
