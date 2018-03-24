ee6486191c30052266657360b65c4960d98bf70c
Xargin <cao1988228@163.com>
2018-03-24 15:31:17 +0800

    first hello world

---
+++ rust/programming_rust/ch1/hello/target/rls/debug/deps/hello-361a3be39b79a777.d
@@ -0,0 +1,5 @@
+/Users/caochunhui/test/rust/programming_rust/ch1/hello/target/rls/debug/deps/hello-361a3be39b79a777.rmeta: src/main.rs
+
+/Users/caochunhui/test/rust/programming_rust/ch1/hello/target/rls/debug/deps/hello-361a3be39b79a777.d: src/main.rs
+
+src/main.rs:
---
+++ rust/programming_rust/ch1/hello/target/rls/debug/libhello.d
@@ -0,0 +1,1 @@
+/Users/caochunhui/test/rust/programming_rust/ch1/hello/target/rls/debug/libhello.rmeta: /Users/caochunhui/test/rust/programming_rust/ch1/hello/src/main.rs
