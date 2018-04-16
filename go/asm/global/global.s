bd3b5744a7ec79d3066455aa92d7bfbc7df55433
Xargin <cao1988228@163.com>
2018-04-16 20:17:46 +0800

    update demo

--- go/asm/global/global.s
+++ go/asm/global/global.s
@@ -1,20 +1,20 @@
 #include "textflag.h"
 
 DATA pi+0(SB)/8, $3.1415926
-DATA one+8(SB)/8, $12345
-DATA two+16(SB)/8, $22222
 GLOBL pi(SB), RODATA, $8
-GLOBL one(SB), RODATA, $16
+DATA two+0(SB)/8, $22222
 GLOBL two(SB), RODATA, $24
-DATA twoy+16(SB)/8, $99
-GLOBL twoy(SB), RODATA, $24
+DATA one+0(SB)/8, $12345
+GLOBL one(SB), RODATA, $8
+DATA three+0(SB)/8, $99
+GLOBL three(SB), RODATA, $8
 
 // 注意这里最后的值，如果 rodata 里已经有其它变量了
 // 就不是所谓的变量 size 了，官方这里严重误导
 
 // func output() (int, float64)
 TEXT ·output(SB),NOSPLIT,$0-16
-    MOVQ one+8(SB), AX
+    MOVQ one+0(SB), AX
     // 注意，是没有办法直接从内存将数据搬到另一块内存的
     // 如果要做这种内存搬运，必须把数据搬到 cpu，再从 cpu 写入到内存
     MOVQ AX, ret+0(FP)
@@ -22,6 +22,6 @@ TEXT ·output(SB),NOSPLIT,$0-16
     // 否则会提示 relocation xxx not found
     MOVQ pi+0(SB), AX
     MOVQ AX, ret+8(FP)
-    MOVQ twoy+16(SB), AX
+    MOVQ three+0(SB), AX
     MOVQ AX, ret+16(FP)
     RET
