a831845f572c09611ab0afc80456749223990cc1
Xargin <cao1988228@163.com>
2018-04-19 00:20:32 +0800

    fix demo

--- go/asm/relative_pos_of_FP_and_SP/global.s
+++ go/asm/relative_pos_of_FP_and_SP/global.s
@@ -3,12 +3,11 @@
 // func output(int) (int, int)
 // 不管这里的 frame size 是 0 还是 8
 // 这个程序的输出都不会变
-// 说明伪寄存器 SP 和 伪寄存器 FP 就是差了 16 个字节？？
-TEXT ·output(SB), $8-24
-	MOVQ arg0+0(FP), AX
-	MOVQ AX, ret1+8(FP)
+TEXT ·output(SB), $16-48
+	MOVQ 32(SP), DX
+	MOVQ DX, ret3+24(FP)
 	MOVQ argx+16(SP), BX
 	MOVQ BX, ret2+16(FP)
-	MOVQ 24(SP), DX
-	MOVQ DX, ret3+24(FP)
+	MOVQ arg0+0(FP), AX
+	MOVQ AX, ret1+8(FP)
 	RET
