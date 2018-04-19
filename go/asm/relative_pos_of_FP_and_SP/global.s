2c3d3a79a46486b92a75610d280a429c9b1ef830
Xargin <cao1988228@163.com>
2018-04-19 12:35:07 +0800

    fix

--- go/asm/relative_pos_of_FP_and_SP/global.s
+++ go/asm/relative_pos_of_FP_and_SP/global.s
@@ -1,9 +1,9 @@
 #include "textflag.h"
 
 // func output(int) (int, int)
-// 不管这里的 frame size 是 0 还是 8
-// 这个程序的输出都不会变
-TEXT ·output(SB), $16-48
+// frame size 是 0 还是 8
+// 会影响程序的输出结果
+TEXT ·output(SB), $8-48
 	MOVQ 32(SP), DX
 	MOVQ DX, ret3+24(FP)
 	MOVQ argx+16(SP), BX
