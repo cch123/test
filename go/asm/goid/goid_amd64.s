0cb9961ddf89fd2bb040a43b1fff847c97a7f7cc
Xargin <cao1988228@163.com>
2018-04-25 01:39:31 +0800

    fix offset

--- go/asm/goid/goid_amd64.s
+++ go/asm/goid/goid_amd64.s
@@ -4,6 +4,7 @@
 TEXT ·getg(SB), NOSPLIT, $0-8
 	get_tls(CX)
 	MOVQ g(CX), AX
-	LEAQ 192(AX), BX // this is the offset
-	MOVQ BX, ret+0(FP)
+	LEAQ 152(AX), BX   // this is the offset
+	MOVQ (BX), DX
+	MOVQ DX, ret+0(FP)
 	RET
