1478778c090c5ccbf7e2aec011942b0bfc137897
Xargin <cao1988228@163.com>
2018-05-01 16:36:45 +0800

    fix

--- go/asm/relative_pos_of_FP_and_SP/global.s
+++ go/asm/relative_pos_of_FP_and_SP/global.s
@@ -1,8 +1,8 @@
 #include "textflag.h"
 
-// func output(int) (int, int)
+// func output(int) (int, int, int)
 // frame size is 0 or 8 will affect the result
-TEXT ·output(SB), $8-48
+TEXT ·output(SB), $8-32
 	MOVQ 24(SP), DX
 	MOVQ DX, ret3+24(FP)
 	MOVQ argx+16(SP), BX
@@ -11,7 +11,7 @@ TEXT ·output(SB), $8-48
 	MOVQ AX, ret1+8(FP)
 	RET
 
-TEXT ·output2(SB), $0-48
+TEXT ·output2(SB), $0-32
 	MOVQ 8(SP), DX
 	MOVQ DX, ret3+24(FP)
 	MOVQ argx+8(SP), BX
