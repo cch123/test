31cad0561af8f04d1c6ccd5b17f9c7c0d92d1a5e
Xargin <cao1988228@163.com>
2018-05-01 13:46:41 +0800

    update

--- go/asm/relative_pos_of_FP_and_SP/global.go
+++ go/asm/relative_pos_of_FP_and_SP/global.go
@@ -5,8 +5,11 @@ import (
 )
 
 func output(int) (int, int, int)
+func output2(int) (int, int, int)
 
 func main() {
 	a, b, c := output(987654321)
+	fmt.Println(a, b, c)
+	a, b, c = output2(98765)
 	fmt.Println(a, b, c)
 }
