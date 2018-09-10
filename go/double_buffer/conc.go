05de502f81e29ee3e232f29761185d8ab6d7be7f
Xargin <cao1988228@163.com>
2018-09-10 22:32:52 +0800

    add double buffer demo

---
+++ go/double_buffer/conc.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type config struct {
+	fk2Opts    map[string]string
+	topic2Opts map[string]string
+	ev2Opts    map[string]string
+	l          sync.RWMutex
+}
+type doublebuffer struct {
+	c1         config
+	c2         config
+	currentIdx int64
+}
+
+var option doublebuffer
+
+func readConfig() (bool, bool, bool) {
+	idx := atomic.LoadInt64(&option.currentIdx)
+	if idx == 0 {
+		option.c1.l.RLock()
+		defer option.c1.l.RUnlock()
+		_, ok1 := option.c1.fk2Opts["1"]
+		_, ok2 := option.c1.topic2Opts["x"]
+		_, ok3 := option.c1.ev2Opts["1"]
+		println(ok1, ok2, ok3)
+		return ok1, ok2, ok3
+	}
+
+	option.c2.l.RLock()
+	defer option.c2.l.RUnlock()
+	_, ok1 := option.c2.fk2Opts["1"]
+	_, ok2 := option.c2.topic2Opts["1"]
+	_, ok3 := option.c2.ev2Opts["x"]
+	println(ok1, ok2, ok3)
+	return ok1, ok2, ok3
+}
+
+func writeToConfig() {
+	fk2Opts := map[string]string{}
+	topic2Opts := map[string]string{}
+	ev2Opts := map[string]string{}
+	for i := 0; i < 10; i++ {
+		fk2Opts[fmt.Sprint(i)] = fmt.Sprint(i)
+		topic2Opts[fmt.Sprint(i)] = fmt.Sprint(i)
+		ev2Opts[fmt.Sprint(i)] = fmt.Sprint(i)
+	}
+	idx := atomic.LoadInt64(&option.currentIdx)
+	if idx == 0 {
+		option.c1.l.Lock()
+		defer option.c1.l.Unlock()
+		option.c1.fk2Opts = fk2Opts
+		option.c1.topic2Opts = topic2Opts
+		option.c1.ev2Opts = ev2Opts
+	} else {
+		option.c2.l.Lock()
+		defer option.c2.l.Unlock()
+		option.c2.fk2Opts = fk2Opts
+		option.c2.topic2Opts = topic2Opts
+		option.c2.ev2Opts = ev2Opts
+	}
+
+	atomic.CompareAndSwapInt64(&option.currentIdx, idx, 1-idx)
+}
+
+func main() {
+	for i := 0; i < 100; i++ {
+		go func() {
+			for {
+				writeToConfig()
+			}
+		}()
+
+		go func() {
+			for {
+				readConfig()
+			}
+		}()
+	}
+	time.Sleep(time.Hour)
+}
