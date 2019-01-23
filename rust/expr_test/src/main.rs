d2cbd63d18c8022d534b42cd5347f0376befa9a7
Xargin <cao1988228@163.com>
2019-01-24 00:16:15 +0800

    完成了基本的bool expr解析

--- rust/expr_test/src/main.rs
+++ rust/expr_test/src/main.rs
@@ -18,30 +18,53 @@ use pest::Parser;
 pub struct ExprParser;
 
 fn main() {
-    /*
-    let expr = ExprParser::parse(Rule::expr, r#"a = "2121""#).expect("parse failed").next().unwrap();
-    dbg!(expr);
-    let expr = ExprParser::parse(Rule::expr, r#"a = 1 and b = 2"#).expect("parse failed").next().unwrap();
-    dbg!(expr);
-    let expr = ExprParser::parse(Rule::expr, "(a=1) and (b=2)").expect("parse failed").next().unwrap();
-    dbg!(expr);
-    let expr = ExprParser::parse(Rule::expr, "a=1 and b=2").expect("parse failed").next().unwrap();
-    dbg!(expr);
-    let expr = ExprParser::parse(Rule::expr, "a=1 and ((b = 2) and c=1)").expect("parse failed").next().unwrap();
-    dbg!(expr);
-    let expr = ExprParser::parse(Rule::expr, "a in 1").expect("parse failed").next().unwrap();
-    dbg!(expr);
-    */
+    let expr = ExprParser::parse(Rule::expr, r#"a = "2121""#)
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
+    let expr = ExprParser::parse(Rule::expr, r#"a = 1 and b = 2"#)
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
+    let expr = ExprParser::parse(Rule::expr, "(a=1) and (b=2)")
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
+    let expr = ExprParser::parse(Rule::expr, "a=1 and b=2")
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
+    let expr = ExprParser::parse(Rule::expr, "a=1 and ((b = 2) and c=1)")
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
+    let expr = ExprParser::parse(Rule::expr, "a in 1")
+        .expect("parse failed")
+        .next()
+        .unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
     let expr = ExprParser::parse(Rule::expr, "(a=1 and ((b = 2) and c=1))")
         .expect("parse failed")
         .next()
         .unwrap();
-    //dbg!(&expr);
-    parse_expr(expr).unwrap();
+    let tree = parse_expr(expr).unwrap();
+    dbg!(tree);
 }
 
 use pest::iterators::Pair;
 
+#[derive(Debug)]
 enum Node {
     AndExpr {
         left: Box<Node>,
@@ -55,63 +78,54 @@ enum Node {
         lhs: String,
         op: String,
         rhs: String,
-    }
+    },
 }
 
-fn parse_expr(expr: Pair<Rule>) -> Result<Node, Error<Rule>> {
-    for record in expr.into_inner() {
-        match record.as_rule() {
-            Rule::expr => {
-                record.into_inner().for_each(|r| {
-                    parse_expr(r).unwrap();
-                });
-            },
-            Rule::or_expr => {
-                let mut iter = record.into_inner();
-                let left = iter.next().unwrap();
-                let right = iter.next().unwrap();
-                let left_tree = parse_expr(left).unwrap();
-                let right_tree= parse_expr(right).unwrap();
-                return Ok(Node::OrExpr{
-                    left : Box::new(left_tree),
-                    right: Box::new(right_tree),
-                });
-            }
-            Rule::and_expr => {
-                let mut iter = record.into_inner();
-                let left = iter.next().unwrap();
-                let right = iter.next().unwrap();
-                let left_tree = parse_expr(left).unwrap();
-                let right_tree= parse_expr(right).unwrap();
-                return Ok(Node::AndExpr {
-                    left : Box::new(left_tree),
-                    right: Box::new(right_tree),
-                });
-            },
-            Rule::paren_bool => {
-                return parse_expr(record.into_inner().next().unwrap());
-            }
-            Rule::comp_expr => {
-                let mut field = "".to_string();
-                let mut op="".to_string() ;
-                let mut value = "".to_string() ;
-                record.into_inner().for_each(|r| {
-                    match r.as_rule() {
-                        Rule::field => field = r.as_str().to_string(),
-                        Rule::op => op = r.as_str().to_string(),
-                        Rule::value=> value = r.as_str().to_string(),
-                        _ => unreachable!()
-                    }
-                });
-
-                return Ok(Node::CompExpr {
-                    lhs: field,
-                    op,
-                    rhs: value,
-                })
-            },
-            _ => unreachable!()
+fn parse_expr(record: Pair<Rule>) -> Result<Node, Error<Rule>> {
+    match record.clone().as_rule() {
+        Rule::expr => {
+            return parse_expr(record.into_inner().next().unwrap());
         }
+        Rule::or_expr => {
+            let mut iter = record.into_inner();
+            let (left_tree, right_tree) = (
+                parse_expr(iter.next().unwrap()).unwrap(),
+                parse_expr(iter.next().unwrap()).unwrap(),
+            );
+            return Ok(Node::OrExpr {
+                left: Box::new(left_tree),
+                right: Box::new(right_tree),
+            });
+        }
+        Rule::and_expr => {
+            let mut iter = record.into_inner();
+            let (left_tree, right_tree) = (
+                parse_expr(iter.next().unwrap()).unwrap(),
+                parse_expr(iter.next().unwrap()).unwrap(),
+            );
+            let res = Node::AndExpr {
+                left: Box::new(left_tree),
+                right: Box::new(right_tree),
+            };
+            return Ok(res);
+        }
+        Rule::paren_bool => {
+            return parse_expr(record.into_inner().next().unwrap());
+        }
+        Rule::comp_expr => {
+            let mut iter = record.into_inner();
+            let (field, op, value) = (
+                iter.next().unwrap().as_str().to_string(),
+                iter.next().unwrap().as_str().to_string(),
+                iter.next().unwrap().as_str().to_string(),
+            );
+
+            return Ok(Node::CompExpr {
+                lhs: field,
+                op,
+                rhs: value,
+            });
+        }
+        _ => unreachable!(),
     }
-    unreachable!()
 }
