1731a1b0d328d06c452a2f44b966acc5b27a5ea3
Xargin <cao1988228@163.com>
2019-01-23 18:57:17 +0800

    add basic parse code

--- rust/expr_test/src/main.rs
+++ rust/expr_test/src/main.rs
@@ -10,8 +10,8 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
 use pest::error::Error;
+use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "expr.pest"]
@@ -32,35 +32,70 @@ fn main() {
     let expr = ExprParser::parse(Rule::expr, "a in 1").expect("parse failed").next().unwrap();
     dbg!(expr);
     */
-    let expr = ExprParser::parse(Rule::expr, "(a=1 and ((b = 2) and c=1))").expect("parse failed").next().unwrap();
+    let expr = ExprParser::parse(Rule::expr, "(a=1 and ((b = 2) and c=1))")
+        .expect("parse failed")
+        .next()
+        .unwrap();
     //dbg!(&expr);
     parse_expr(expr).unwrap();
 }
 
 use pest::iterators::Pair;
-fn parse_expr(expr : Pair<Rule>) -> Result<(), Error<Rule>> {
+
+enum Node {
+    AndExpr {
+        left: Box<Node>,
+        right: Box<Node>,
+    },
+    OrExpr {
+        left: Box<Node>,
+        right: Box<Node>,
+    },
+    CompExpr {
+        lhs: String,
+        op: String,
+        rhs: String,
+    },
+    Null,
+}
+
+fn parse_expr(expr: Pair<Rule>) -> Result<Node, Error<Rule>> {
     for record in expr.into_inner() {
         match record.as_rule() {
             Rule::expr => {
-                record.into_inner().for_each(|r|{
-                    parse_expr(r);
+                record.into_inner().for_each(|r| {
+                    parse_expr(r).unwrap();
+                });
+            }
+            Rule::or_expr => {}
+            Rule::and_expr => {
+                let mut iter = record.into_inner();
+                let left = iter.next().unwrap(); //.into_inner().next().unwrap();
+                let right = iter.next().unwrap();
+                println!("{:?}", left);
+                println!("{:?}", right);
+                return Ok(Node::AndExpr {
+                    left : Box::new(Node::Null),
+                    right: Box::new(Node::Null),
                 });
             },
-            Rule::or_expr => {
-                dbg!(record);
-            },
-            Rule::and_expr => {
-                dbg!(record);
-            },
             Rule::paren_bool => {
-                record.into_inner().for_each(|r|{
-                    dbg!(r);
+                record.into_inner().for_each(|r| {
+                    println!("{:?}, {:?}", r.as_str(), r.as_rule());
+                    parse_expr(r).unwrap();
                 });
             }
             Rule::comp_expr => {
+                return Ok(Node::CompExpr {
+                    lhs: "abc".to_string(),
+                    op: "=".to_string(),
+                    rhs: "aaa".to_string(),
+                })
+            },
+            _ => {
+                println!("fff{:?}", record);
             }
-            _ => unreachable!()
         }
     }
-    Ok(())
+    Ok(Node::Null)
 }
