fe380429cfe03ba5f32726d4acc361e6699eed17
Xargin <cao1988228@163.com>
2019-02-02 23:58:44 +0800

    remove useless

--- rust/prec_climber_test/src/main.rs
+++ rust/prec_climber_test/src/main.rs
@@ -3,22 +3,17 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::error::Error;
-
-use pest::Parser;
 use pest::iterators::Pair;
 use pest::prec_climber::{Assoc, Operator, PrecClimber};
+use pest::Parser;
 
 fn main() {
-   // let input = std::env::args().nth(1).expect("args[1] is not found");
-    let input = "1+2+3";
-    match do_parse(&input) {
-        Ok(expression) => println!("{:#?}", expression),
-        Err(err) => println!("failed"),
-    }
+    let input = "1+2+3+4+3223*2";
+    let mut pairs = ExprParser::parse(Rule::expression, input).expect("fuck");
+    let pair = pairs.next().unwrap();
+    println!("{:#?}", pair);
+    println!("{:#?}", into_expression(pair));
 }
-
-
 
 #[derive(Debug)]
 enum Expression {
@@ -28,9 +23,9 @@ enum Expression {
 
 impl Expression {
     fn infix<L, R>(op: InfixOp, lhs: L, rhs: R) -> Self
-        where
-            L: Into<Expression>,
-            R: Into<Expression>,
+    where
+        L: Into<Expression>,
+        R: Into<Expression>,
     {
         Expression::Infix(op.into(), Box::new(lhs.into()), Box::new(rhs.into()))
     }
@@ -46,26 +41,16 @@ enum InfixOp {
     Power,
 }
 
-
-
-const _GRAMMAR: &str = include_str!("grammar.pest");
-
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct ExprParser;
 
-fn do_parse<'s>(input: &'s str) -> Result<Expression, Error<String>> {
-    let mut pairs = ExprParser::parse(Rule::expression, input).expect("fuck");
-    let pair = pairs.next().unwrap();
-    Ok(into_expression(pair))
-}
-
 fn into_expression(pair: Pair<Rule>) -> Expression {
     let climber = PrecClimber::new(vec![
-        Operator::new(Rule::plus, Assoc::Left) |
-            Operator::new(Rule::minus, Assoc::Left),
-        Operator::new(Rule::times, Assoc::Left) | Operator::new(Rule::divide, Assoc::Left) |
-            Operator::new(Rule::modulus, Assoc::Left),
+        Operator::new(Rule::plus, Assoc::Left) | Operator::new(Rule::minus, Assoc::Left),
+        Operator::new(Rule::times, Assoc::Left)
+            | Operator::new(Rule::divide, Assoc::Left)
+            | Operator::new(Rule::modulus, Assoc::Left),
         Operator::new(Rule::power, Assoc::Right),
     ]);
 
