09134a9d10d721bc700fd91c74b1367cb8baa0b6
Xargin <cao1988228@163.com>
2019-02-05 20:39:59 +0800

    optimize structs

--- rust/nom/basic/src/main.rs
+++ rust/nom/basic/src/main.rs
@@ -1,22 +1,34 @@
 #[macro_use]
 extern crate nom;
-use nom::{do_parse, named, take_while, multispace};
 use nom::types::CompleteStr;
+use nom::{do_parse, multispace, named, take_while};
 
 #[derive(Debug, PartialEq)]
-pub struct CompExpr <'a>{
-    pub lhs: CompleteStr<'a>,
-    pub op: CompleteStr<'a>,
-    pub rhs: CompleteStr<'a>,
+pub enum BoolExpr<'a> {
+    CompExpr {
+        lhs: CompleteStr<'a>,
+        op: CompleteStr<'a>,
+        rhs: CompleteStr<'a>,
+    },
+    AndExpr {
+        left: Box<BoolExpr<'a>>,
+        right: Box<BoolExpr<'a>>,
+    },
+    OrExpr {
+        left: Box<BoolExpr<'a>>,
+        right: Box<BoolExpr<'a>>,
+    },
 }
 
 named!(ident<CompleteStr, CompleteStr>,
   take_while!(|c: char| c.is_ascii_alphanumeric())
 );
 
-
 named!(operator<CompleteStr, CompleteStr>,
-  take_while!(|c: char| match c{ '='|'>'|'<' => true, _ => false})
+  take_while!(|c: char| {
+    if c.is_ascii_alphanumeric() {return true}
+    match c{ '='|'>'|'<' => true, _ => false}
+  })
 );
 
 named!(eat_space<CompleteStr, CompleteStr>,
@@ -24,25 +36,24 @@ named!(eat_space<CompleteStr, CompleteStr>,
 );
 
 named!(pub opt_multispace<CompleteStr, Option<CompleteStr>>,
-       opt!(complete!(multispace))
+  opt!(complete!(multispace))
 );
 
 named!(number<CompleteStr, CompleteStr>,
   take_while1!(|c : char| c.is_ascii_digit())
 );
 
-named!(comp_expr<CompleteStr, CompExpr>,
+named!(comp_expr<CompleteStr, BoolExpr>,
   do_parse!(
-    lhs:   ident >>
-    opt_multispace >>
-    op:   operator >>
-    opt_multispace >>
+    lhs:   ident >> opt_multispace >>
+    op:   operator >> opt_multispace >>
     rhs:   number>>
-    (CompExpr{ lhs, op, rhs})
+    (BoolExpr::CompExpr{ lhs, op, rhs})
   )
 );
 
 fn main() {
-    println!("{:#?}", comp_expr(CompleteStr("aa =1 ")));
+    println!("{:#?}", comp_expr(CompleteStr("aa >= 1")));
     println!("{:#?}", comp_expr(CompleteStr("b = 2")));
+    println!("{:#?}", comp_expr(CompleteStr("b in 2")));
 }
