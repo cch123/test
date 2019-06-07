a91c6dd649f353d5a8ffd3a50a3808434707adb3
Xargin <cao1988228@163.com>
2019-06-08 01:40:42 +0800

    add more routes for actix web

--- rust/actix/actix_test/src/main.rs
+++ rust/actix/actix_test/src/main.rs
@@ -2,12 +2,26 @@
 extern crate actix_web;
 use http::StatusCode;
 
-use actix_web::{error, guard, middleware, Error, HttpRequest, HttpResponse, Result};
-use actix_web::{web, App, HttpServer, Responder};
+// use actix_web::{error, guard, middleware, Error, HttpRequest, HttpResponse, Result};
+use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse};//, Responder};
 use std::collections::HashMap;
-use actix_web::test::read_response_json;
+//use actix_web::test::read_response_json;
 
-fn with_param(req: HttpRequest) -> HttpResponse {
+#[get("/fuck")]
+fn index3(req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body("oh jack, oh rose")
+}
+
+#[post("/setplay")]
+fn index4(req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("ook")
+        .body("oh yeas")
+}
+
+fn index2(req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/plain")
         .body("abcdefg");
@@ -20,7 +34,7 @@ fn with_param(req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().status(StatusCode::INTERNAL_SERVER_ERROR).json(m)
 }
 
-fn without_param(req: HttpRequest, path: web::Path<(String,)>) -> HttpResponse {
+fn index(req: HttpRequest, path: web::Path<(String,)>) -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/plain")
         .body(format!("Hello fk!"))
@@ -29,9 +43,11 @@ fn without_param(req: HttpRequest, path: web::Path<(String,)>) -> HttpResponse {
 fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/user/{name}").route(web::get().to(with_param)))
+            .service(web::resource("/user/{name}").route(web::get().to(index)))
             .service(web::resource("/index.html").to(|| "Hello world!"))
-            .service(web::resource("/").to(with_param))
+            .service(web::resource("/").to(index2))
+            .service(index3)
+            .service(index4)
     })
     .bind("0.0.0.0:8080")?
     .run()
