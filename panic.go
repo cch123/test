fe2ffa0cf6b643af0c5acb54406b33fa431a3126
Xargin <cao1988228@163.com>
2019-06-25 17:18:29 +0800

    fix

--- panic.go
+++ panic.go
@@ -18,3 +18,34 @@ func main() {
 }
 
 //wrk -c200 -t20 -d30s  http://127.0.0.1:9090
+/*
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "reflect"
+)
+
+func panic(w http.ResponseWriter, r *http.Request) {
+    server := r.Context().Value(http.ServerContextKey).(*http.Server)
+    v := reflect.ValueOf(*server)
+
+    for i := 0; i < v.NumField(); i++ {
+        if name := v.Type().Field(i).Name; name != "activeConn" {
+            continue
+        }
+        fmt.Println(v.Field(i))
+    }
+}
+
+func main() {
+    http.HandleFunc("/", panic)
+    err := http.ListenAndServe(":9090", nil)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+wrk -c200 -t20 -d30s  http://127.0.0.1:9090
+*/
