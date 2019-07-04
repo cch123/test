2125a0e0ef25f1a4132a1f4e0db1256ff4a9d189
Xargin <cao1988228@163.com>
2019-07-04 19:22:59 +0800

    add reqwest demo

--- rust/actix/actix_test/src/main.rs
+++ rust/actix/actix_test/src/main.rs
@@ -2,36 +2,16 @@
 extern crate actix_web;
 use http::StatusCode;
 
-// use actix_web::{error, guard, middleware, Error, HttpRequest, HttpResponse, Result};
-use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse};//, Responder};
+use actix_web::{web, App, HttpServer,HttpRequest, HttpResponse};//, Responder};
 use std::collections::HashMap;
-//use actix_web::test::read_response_json;
+use actix_http::KeepAlive;
 
-#[get("/fuck")]
-fn index3(req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("application/json")
-        .body("oh jack, oh rose")
-}
-
-#[post("/setplay")]
-fn index4(req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("ook")
-        .body("oh yeas")
-}
 
 fn index2(req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/plain")
-        .body("abcdefg");
+        .body("hello")
 
-    let mut m = HashMap::new();
-    m.insert("fuck", "you");
-    HttpResponse::Ok()
-        .json(&m);
-
-    HttpResponse::Ok().status(StatusCode::INTERNAL_SERVER_ERROR).json(m)
 }
 
 fn index(req: HttpRequest, path: web::Path<(String,)>) -> HttpResponse {
@@ -43,12 +23,12 @@ fn index(req: HttpRequest, path: web::Path<(String,)>) -> HttpResponse {
 fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(web::resource("/user/{name}").route(web::get().to(index)))
-            .service(web::resource("/index.html").to(|| "Hello world!"))
+            //.service(web::resource("/user/{name}").route(web::get().to(index)))
+            //.service(web::resource("/index.html").to(|| "Hello world!"))
             .service(web::resource("/").to(index2))
-            .service(index3)
-            .service(index4)
+            //.service(index3)
+            //.service(index4)
     })
-    .bind("0.0.0.0:8080")?
+    .bind("0.0.0.0:9999")?
     .run()
 }
---
+++ rust/reqwest/example/src/main.rs
@@ -0,0 +1,27 @@
+use reqwest::Client;
+use reqwest::StatusCode;
+
+fn main() -> Result<(), Box<::std::error::Error>> {
+    let client = Client::new();
+
+    let mut resp = client
+        .post("http://httpbin.org/post")
+        .send()?;
+
+    /*
+    match resp.status() {
+        StatusCode::OK => println!("success!"),
+        StatusCode::PAYLOAD_TOO_LARGE => {
+            println!("Request payload is too large!");
+        }
+        s => println!("Received response status: {:?}", s),
+    };
+    */
+    /*
+    let js: serde_json::Value = resp.json()?;
+    println!("{}", js);
+    */
+    //println!("{}", resp.json()?);
+    println!("{}", resp.json::<serde_json::Value>()?);
+    Ok(())
+}
