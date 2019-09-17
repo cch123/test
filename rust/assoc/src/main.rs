090c73cece2b37a0ea5c706ee01c406c0923ac58
Xargin <cao1988228@163.com>
2019-09-17 21:05:34 +0800

    update

--- rust/assoc/src/main.rs
+++ rust/assoc/src/main.rs
@@ -1,12 +1,47 @@
 trait Graph {
     type Node; // associated type
     type Edge; // associated type
-    
+
     fn get_visited(&self, n: &Self::Node) -> bool;
     fn set_visited(&mut self, n: &Self::Node);
-    fn get_successors(&self, n: &Self::Node) -> [Self::Node];
+    fn get_successors(&self, n: &Self::Node) -> Vec<Self::Node>;
+}
+
+impl<N, E> dyn Graph<Node=N, Edge=E>
+{
+    fn get_visited(&self, n: N) -> bool {
+        true
+    }
+
+    fn set_visited(&mut self, n: N) {}
+
+    fn get_successors(&self, n: N) -> Vec<N> {
+        vec![]
+    }
+}
+
+impl Graph for i32 {
+    type Node = i32;
+    type Edge = i32;
+    fn get_visited(&self, n: &Self::Node) -> bool {
+        true
+    }
+
+    fn set_visited(&mut self, n: &Self::Node) {}
+
+    fn get_successors(&self, n: &Self::Node) -> Vec<Self::Node> {
+        vec![]
+    }
+}
+
+// 完全独立的泛型函数
+fn dfs<G>(g: &mut G) -> Vec<G::Node>
+where
+    G: Graph,
+{
+    vec![]
 }
 
 fn main() {
-    println!("Hello, world!");
+    //let mut g = Graph<i32,i32>::new();
 }
