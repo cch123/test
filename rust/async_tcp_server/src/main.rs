b3d59c37f78ba5382d3a9d753358000b8d850dee
Xargin <cao1988228@163.com>
2019-11-10 22:38:10 +0800

    an easy async tcp server echo server with shutdown

---
+++ rust/async_tcp_server/src/main.rs
@@ -0,0 +1,31 @@
+//! Hello world example for Rust.
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let addr = "0.0.0.0:11333";
+    let mut listener = TcpListener::bind(&addr).await?;
+    loop {
+        let (mut conn, remote) = listener.accept().await?;
+        dbg!(remote);
+
+        tokio::spawn(async move {
+            let mut buf : [u8;10]= [0; 10];
+            loop {
+                let n = conn.read(&mut buf).await.expect("expected data, but EOF");
+                
+                if n == 0 {
+                    break;
+                }
+                println!("received packet, len {}, content : {:?}", n, buf);
+
+                conn.write_all(&buf[0..n]).await.expect("write faile");
+                // 如果收到 quit，关闭连接
+                if &buf[0..n] == b"quit\r\n" {
+                    conn.shutdown(std::net::Shutdown::Both).expect("shutdown fuck!");
+                }
+            }
+        });
+    }
+}
