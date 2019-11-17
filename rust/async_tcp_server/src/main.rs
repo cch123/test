67afd154fa4d613e2f94cd179ab8b5f3ce31f9c7
Xargin <cao1988228@163.com>
2019-11-17 15:22:53 +0800

    len -> body protocol

--- rust/async_tcp_server/src/main.rs
+++ rust/async_tcp_server/src/main.rs
@@ -35,25 +35,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let num_bytes = buf[0..n]
                     .iter()
                     .map(|e| *e)
-                    .take_while(|&e| e > b'0' && e < b'9')
+                    .take_while(|&e| e >= b'0' && e <= b'9')
                     .collect::<Vec<_>>();
 
                 //println!("string from request: {:?}", String::from_utf8(num_bytes));
 
                 match num_bytes.len() {
-                    e if e > 512 => {
+                    l if l > 20 => {
+                        println!("shutdown because request too long");
                         conn.shutdown(std::net::Shutdown::Both)
-                            .expect("shutdown because req too long");
+                            .expect("shutdown failed");
                     }
-                    _ => {
-                        let req_len = std::str::from_utf8(&num_bytes)
+                    l if l == 0 => {
+                        println!("shutdown because no request length description");
+                        conn.shutdown(std::net::Shutdown::Both)
+                            .expect("shutdown failed");
+                    }
+                    l => {
+                        let req_len = String::from_utf8(num_bytes)
                             .unwrap()
-                            .parse::<i32>()
+                            .parse::<i64>()
                             .unwrap();
-                        let req = String::from_utf8(buf[num_bytes.len() + 1..n].to_vec()).unwrap();
-                        if req.trim().len() != req_len as usize {
+
+                        let buf_vec = buf[l+1..n].to_vec();
+                        let req_body = std::str::from_utf8(&buf_vec).unwrap();
+
+                        let mut req_body = req_body.trim_start().to_string();
+
+                        while req_body.len() < (req_len as usize) {
+                            let n = conn.read(&mut buf).await.expect("expected data, but EOF");
+                            println!("fyckk");
+
+                            if n == 0 {
+                                println!("fyc");
+                                break;
+                            }
+                            req_body.push_str(std::str::from_utf8(&buf[..n].to_vec()).unwrap());
+                        }
+                        req_body = req_body.trim_end().to_string();
+
+                        if req_body.len() != (req_len as usize) {
                             println!("wrong len of request body");
                         }
+
+                        dbg!(req_len, req_body.clone(), req_body.len());
                     }
                 }
             }
