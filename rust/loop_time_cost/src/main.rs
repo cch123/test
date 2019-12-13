caad18043e19eb83850cf440673bb994579b9cc7
Xargin <cao1988228@163.com>
2019-12-13 13:06:02 +0800

    add push time cost test

--- rust/loop_time_cost/src/main.rs
+++ rust/loop_time_cost/src/main.rs
@@ -1,9 +1,23 @@
-use std::time::{Duration, Instant};
+use std::time::Instant;
+use std::collections::LinkedList;
 
 fn main() {
     let start = Instant::now();
-    (0..1000000).for_each(|_|{
+    let mut v = Vec::with_capacity(0);
+    (0..1000000).for_each(|i|{
          // do nothing
+         v.push(i);
     });
     dbg!(start.elapsed());
+    dbg!(v.len());
+
+    let start = Instant::now();
+    let mut list = LinkedList::new();
+    (0..1000000).for_each(|i|{
+         // do nothing
+         //list.push(i);
+         list.push_back(i);
+    });
+    dbg!(start.elapsed());
+    dbg!(list.len());
 }
