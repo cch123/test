75755f8e4622ea8443ec649407b98f7130875b6e
Xargin <cao1988228@163.com>
2019-12-23 14:17:50 +0800

    update

--- rust/net2_test/src/main.rs
+++ rust/net2_test/src/main.rs
@@ -14,20 +14,26 @@ use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let tcp = TcpBuilder::new_v4().unwrap();
     let vv = tcp.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap().listen(128).unwrap();
     let l = TcpListener::from_std(vv);
     dbg!(&l);
-    main2().await;
+
+    client_reuseport().await?;
+    Ok(())
 }
 
-async fn main2() -> Result<(), Box<dyn std::error::Error>>{
+async fn client_reuseport() -> Result<(), Box<dyn std::error::Error>>{
     let tcp = TcpBuilder::new_v4().unwrap();
     tcp.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap();
-    let mut stream = tcp.connect("127.0.0.1:80").unwrap();
-    dbg!(&stream);
-    let mut stream = TcpStream::from_std(stream)?;
+    let stream = TcpStream::from_std(tcp.connect("220.181.38.148:80").unwrap())?;
+
+    let tcp = TcpBuilder::new_v4().unwrap();
+    tcp.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap();
+    let stream2 = TcpStream::from_std(tcp.connect("111.13.149.108:80").unwrap())?;
+
+    dbg!(&stream, stream2);
     Ok(())
 }
 
