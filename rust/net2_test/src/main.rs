41a1391365133caf94958dbc746d4677b9bc2331
Xargin <cao1988228@163.com>
2019-12-23 14:12:15 +0800

    add net2 reuse port

--- rust/net2_test/src/main.rs
+++ rust/net2_test/src/main.rs
@@ -1,12 +1,33 @@
+/** 
+  use net2::TcpBuilder;
+
+let tcp = TcpBuilder::new_v4().unwrap();
+tcp.reuse_address(true).unwrap()
+   .only_v6(false).unwrap();
+
+let mut stream = tcp.connect("127.0.0.1:80").unwrap();
+*/
+
 use net2::unix::UnixTcpBuilderExt;
 use net2::TcpBuilder;
 use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-    let v = TcpBuilder::new_v4().unwrap();
-    let vv = v.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap().listen(128).unwrap();
+    let tcp = TcpBuilder::new_v4().unwrap();
+    let vv = tcp.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap().listen(128).unwrap();
     let l = TcpListener::from_std(vv);
     dbg!(&l);
+    main2().await;
+}
+
+async fn main2() -> Result<(), Box<dyn std::error::Error>>{
+    let tcp = TcpBuilder::new_v4().unwrap();
+    tcp.reuse_port(true).unwrap().bind("0.0.0.0:12345").unwrap();
+    let mut stream = tcp.connect("127.0.0.1:80").unwrap();
+    dbg!(&stream);
+    let mut stream = TcpStream::from_std(stream)?;
+    Ok(())
 }
 
