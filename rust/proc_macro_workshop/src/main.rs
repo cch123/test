04110f510c5a3da923a3a0ad9928becd7ab8010e
Xargin <cao1988228@163.com>
2019-12-24 17:22:53 +0800

    add list macro

---
+++ rust/proc_macro_workshop/src/main.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct Node {
+    pub val: i32,
+    pub next: Option<Box<Node>>,
+}
+
+impl Node {
+    pub fn new(val: i32) -> Self {
+        Node {next : None, val}
+    }
+}
+
+macro_rules! linkedlist {
+    () => {
+        None
+    };
+    ($ ($x:expr), *) => {
+        {
+            let mut dummy_head = Box::new(Node::new(0));
+            let mut cursor = &mut dummy_head;
+            $(
+                cursor.next = Some(Box::new($crate::Node::new($x)));
+                cursor = cursor.next.as_mut().unwrap();
+            )*
+            drop(cursor);
+
+            dummy_head.next
+        }
+    };
+}
+
+fn main() {
+    let x = linkedlist![1,2,3,4,5];
+    dbg!(x);
+}
