efe5e42e3dcc0a1f85f3f94424078e948cc8167f
Xargin <buaa.cch@live.com>
2020-04-23 15:40:51 +0800

    add http 2 demo

---
+++ go/http/http2/http2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"log"
+	_ "net/http/pprof"
+
+	"net/http"
+)
+
+func sayhello(wr http.ResponseWriter, r *http.Request) {
+	// 标准库没有办法在写入后，再从responsewriter里读出来
+	// 蛋疼
+	//	wr.WriteHeader(404)
+	//	wr.WriteHeader(1)
+	wr.Header()["Content-Type"] = []string{"application/json"}
+	//wr.Header().Set()
+	io.WriteString(wr, "hello")
+
+	//	panic(1)
+
+	//	fmt.Println(wr.Header())
+}
+
+func main() {
+	http.HandleFunc("/", sayhello)
+	err := http.ListenAndServeTLS(":9090", "./server.crt", "./server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ go/http/http2/readme.md
@@ -0,0 +1,10 @@
+```
+~ ❯❯❯ curl -i https://localhost:9090 -k
+HTTP/2 200
+content-type: application/json
+content-length: 5
+date: Thu, 23 Apr 2020 07:40:12 GMT
+
+hello
+
+```
