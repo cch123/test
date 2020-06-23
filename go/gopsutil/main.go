d321dc8531b9c589b8a7fb5a3e5edcd124bdf8f7
Xargin <buaa.cch@live.com>
2020-06-23 16:22:27 +0800

    add cpu usage demo

--- go/gopsutil/main.go
+++ go/gopsutil/main.go
@@ -2,16 +2,44 @@ package main
 
 import (
     "fmt"
+	"os"
+	"time"
 
     "github.com/shirou/gopsutil/mem"
+    "github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/process"
 )
 
+func cpuTest2() {
+	p, err := process.NewProcess(int32(os.Getpid()))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(p.CPUPercent())
+}
+
+func cpuTest() {
+	rList, _ := cpu.Percent(time.Second, true)
+	for _, r := range rList {
+		fmt.Println(r)
+    }
+}
+
+func memTest() {
+	v, _ := mem.VirtualMemory()
+
+	// almost every return value is a struct
+	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+
+	// convert to JSON. String() is also implemented
+	fmt.Println(v)
+}
+
 func main() {
-    v, _ := mem.VirtualMemory()
-
-    // almost every return value is a struct
-    fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
-
-    // convert to JSON. String() is also implemented
-    fmt.Println(v)
+	// cpuTest()
+	// memTest()
+	for {
+		cpuTest2()
+	}
 }
