402bb72b586c52a83c2c9a1c3c11240df3e2e50a
Xargin <cao1988228@163.com>
2020-10-20 19:40:46 +0800

    update

--- go/tls/client.go
+++ go/tls/client.go
@@ -3,33 +3,54 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/juju/ratelimit"
 )
 
 func main() {
-
-	conf := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-
-	conn, err := tls.Dial("tcp", "127.0.0.1:443", conf)
+	connNum, err := strconv.ParseInt(os.Args[1], 10, 64)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
 
-	n, err := conn.Write([]byte("/hello\n"))
+	qps, err := strconv.ParseInt(os.Args[2], 10, 64)
 	if err != nil {
-		fmt.Println(err, n)
+		fmt.Println(err)
 		return
 	}
 
-	buf := make([]byte, 100)
-	n, err = conn.Read(buf)
-	if err != nil {
-		fmt.Println(err, n)
-		return
+	bucket := ratelimit.NewBucket(time.Second/time.Duration(qps), qps)
+
+	connList := []*http.Client{}
+	for i := 0; i < int(connNum); i++ {
+		connList = append(connList, &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig:     &tls.Config{InsecureSkipVerify: true},
+				IdleConnTimeout:     0,
+				MaxIdleConns:        1,
+				MaxIdleConnsPerHost: 1,
+			},
+		})
 	}
 
-	println(string(buf[:n]))
+	for i := 0; ; i++ {
+		bucket.Wait(1)
+		conn := connList[i%len(connList)]
+		go func() {
+			if resp, e := conn.Get("https://localhost:4443"); e != nil {
+				fmt.Println(e)
+			} else {
+				defer resp.Body.Close()
+				io.Copy(ioutil.Discard, resp.Body)
+			}
+		}()
+	}
+
 }
--- go/tls/server.go
+++ go/tls/server.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"crypto/tls"
 	"fmt"
-	"net"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		resp.Write(make([]byte, 16380))
+	})
 
-	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	err := http.ListenAndServeTLS(":4443", "server.crt", "server.key", nil)
 	if err != nil {
 		fmt.Println(err)
-		return
-	}
-
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err := tls.Listen("tcp", ":443", config)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer ln.Close()
-
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		go handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	r := bufio.NewReader(conn)
-	for {
-		msg, err := r.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		println(msg)
-
-		n, err := conn.Write([]byte("world"))
-		if err != nil {
-			fmt.Println(err, n)
-			return
-		}
 	}
 }
