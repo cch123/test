3f2819fe7b75269232594cd2f70fede464bc4e16
Xargin <cao1988228@163.com>
2020-11-09 17:18:21 +0800

    fix dup register

--- go/epoll/easygo/easygo.go
+++ go/epoll/easygo/easygo.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net"
-
 	"github.com/mailru/easygo/netpoll"
+	"io"
+	"net"
+	"net/http"
 )
 
 func main() {
@@ -18,41 +20,57 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
 	l, err := net.Listen("tcp4", ":10101")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	for {
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		tcpConn := c.(*net.TCPConn)
-		f, err := tcpConn.File()
+
+		f, err := c.(*net.TCPConn).File()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		var buf = make([]byte, 1024)
-		var off = 0
-		err = poller.Start(netpoll.NewDesc(f.Fd(), netpoll.EventRead),
+		fmt.Println(f.Fd())
+		desc := netpoll.NewDesc(f.Fd(), netpoll.EventRead)
+		err = poller.Start(desc,
 			func(ev netpoll.Event) {
-
 				fmt.Println("event come, event go", ev)
 				fmt.Println(ev.String())
 
-				n, err := tcpConn.Read(buf[off:])
-				fmt.Println("tcp read", n, err, string(buf))
-
-				if off+n == cap(buf) {
-					bufNew := make([]byte, cap(buf)*2)
-					copy(bufNew, buf)
+				req, err := http.ReadRequest(bufio.NewReader(f))
+				if err == io.EOF {
+					fmt.Println("read EOF")
+					defer f.Close()
+					poller.Stop(desc)
+					return
 				}
 
-				off = n
+				if err != nil {
+					fmt.Println(err, "read request failed")
+					return
+				}
+
+				fmt.Println(req)
+				resp := http.Response{
+					StatusCode : http.StatusOK,
+					ProtoMajor: 1,
+					ProtoMinor: 1,
+				}
+				err = resp.Write(f)
+				if err != nil {
+					fmt.Println(err, "write failed")
+					return
+				}
 			},
 		)
 
