8c25575696113a4414b535dc2e0e5167ee0f5572
xargin <xargin@xargins-MacBook-Pro.local>
2023-08-19 17:10:05 +0800

    set reflect

--- go/reflect/init/main.go
+++ go/reflect/init/main.go
@@ -15,7 +15,15 @@ func convertToNonEmpty(input any) any {
 	if input == nil || reflect.ValueOf(input).IsNil() {
 		// init the input and return
 		result = reflect.New(reflect.TypeOf(input).Elem()).Interface()
+	} else {
+		return input
 	}
+
+	name := reflect.ValueOf(result).Elem().FieldByName("Name")
+	age := reflect.ValueOf(result).Elem().FieldByName("Age")
+
+	name.SetString("xargin")
+	age.SetInt(123)
 
 	return result
 }
